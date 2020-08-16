12cdaf0cf39c9d2cf2461c5835a0bf59bce08801
yusukem99 <yusuke.m99@gmail.com>
2020-08-16 16:46:13 +0900

    サンプル作成中

--- demo/js/index.js
+++ demo/js/index.js
@@ -49,24 +49,79 @@ let channelOption = {
     ],
     callback_channel: {
         onmessage : (id, account, label, message) => {
+            console.log(message)
         }
     }
 }
 
+var array = new Uint32Array(2);
+let localid = window.crypto.getRandomValues(array)[0].toString();
+let remoteid = window.crypto.getRandomValues(array)[1].toString();
+
+const remoteaccount = {}
 let live = new Live( emitTo, onicedisconnect, null, channelOption );
+
+let button = document.getElementById('debug')
+let makeoffer = document.getElementById('makeoffer')
 let video = document.getElementById('myvideo');
-video.addEventListener( "loadedmetadata", function (e) {
-    var width = this.videoWidth,height = this.videoHeight;
-    canvasElm.width = width;
-    canvasElm.height = height;
-}, false );
-const canvasElm = document.createElement('canvas')
-document.body.appendChild( canvasElm );
+let setdata = document.getElementById('setdata');
+let setoffer = document.getElementById('setoffer');
+let setanswer = document.getElementById('setanswer');
+
+button.onclick = () => {
+    console.log(live)
+}
+
+makeoffer.onclick = () => {
+    live.makeOffer('remote', remoteaccount)
+}
+
+setoffer.onclick = () => {
+    live.setOffer('remote', {}, {
+        type: 'offer',
+        sdp: setdata.value
+    })
+}
+
+setanswer.onclick = () => {
+    live.setAnswer('remote', {
+        type: 'answer',
+        sdp: setdata.value
+    })
+}
 
 function emitTo(id, msg) {
     msg.sendto = id;
     socket.emit('message', msg);
 }
+/*
+function emitTo(id, msg) {
+    console.log(id);
+    console.log(msg);
+    const div = document.createElement('div');
+    const text = document.createElement('div');
+    text.textContent = `${id} ${msg.type}`;
+    div.appendChild(text);
+    const textarea = document.createElement('textarea');
+    switch (msg.type) {
+        case 'offer':
+            break;
+        case 'answer':     
+            break;
+        case "candidate":
+            if(!msg.ice){
+                textarea.value = live.peerConnections.getConnection(id).peer.localDescription.sdp
+            }
+            break;
+            
+        default:
+            break;
+    }
+
+    div.appendChild(textarea)
+    document.body.appendChild(div)
+}
+*/
 function onicedisconnect(){
     let video = document.getElementById('remote_video');
     video.srcObject = null;
