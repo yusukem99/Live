6d0706d7f6c2a530b3bdbf4b49576745a88990e1
yusukem99 <yusuke.m99@gmail.com>
2020-08-17 10:59:08 +0900

    change to vanilla ice

--- demo/js/index.js
+++ demo/js/index.js
@@ -1,47 +1,5 @@
 import Live from '../node_modules/live/Live.js'
 
-let socket = io.connect(`https://www.webproj.jp:31116/`, {});
-socket.on('connect', (evt) => {
-    console.log('connect');
-    socket.emit('enter', {});
-    socket.on('message', (message) => {
-        console.log(`socket.on message.type: ${message.type}`)
-        let fromId = message.from;
-    
-        if (message.type === 'offer') {
-            let offer = new RTCSessionDescription(message);
-            live.setOffer(fromId, {}, offer);
-        }
-        else if (message.type === 'answer') {
-            let answer = new RTCSessionDescription(message);
-            live.setAnswer(fromId, answer);
-        }
-        else if (message.type === 'candidate') {
-            console.log('Received ICE candidate ...');
-            let candidate = new RTCIceCandidate(message.ice);
-            live.addIceCandidate(fromId, candidate);
-        }
-        else if (message.type === 'call me') {
-            live.makeOffer(fromId, {});
-
-        }
-        else if (message.type === 'bye') {
-            if (live.isConnectedWith(fromId)) {
-                live.stopConnection(fromId);
-            }
-        }
-    });
-    socket.on('entered', (message) => {
-        if( message.isSuccess ){
-            socket.emit('message', {
-                type: 'call me'
-            });
-        }else{
-            console.log('you are not member of: ' + message.ch_id);
-        }
-    });
-});
-
 let channelOption = {
     labels: [
         "chat",
@@ -91,18 +49,14 @@ setanswer.onclick = () => {
 }
 
 function emitTo(id, msg) {
-    msg.sendto = id;
-    socket.emit('message', msg);
-}
-/*
-function emitTo(id, msg) {
     console.log(id);
     console.log(msg);
     const div = document.createElement('div');
     const text = document.createElement('div');
-    text.textContent = `${id} ${msg.type}`;
-    div.appendChild(text);
     const textarea = document.createElement('textarea');
+    div.appendChild(text);
+    div.appendChild(textarea)
+    text.textContent = `message send to ${id} ${msg.type}`;
     switch (msg.type) {
         case 'offer':
             break;
@@ -111,17 +65,17 @@ function emitTo(id, msg) {
         case "candidate":
             if(!msg.ice){
                 textarea.value = live.peerConnections.getConnection(id).peer.localDescription.sdp
+                document.body.appendChild(div)
+            }
+            else{
+                console.log(msg)
             }
             break;
-            
         default:
             break;
     }
-
-    div.appendChild(textarea)
-    document.body.appendChild(div)
 }
-*/
+
 function onicedisconnect(){
     let video = document.getElementById('remote_video');
     video.srcObject = null;
